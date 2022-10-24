42558ca54ff61d514ec4b0cfaf79e67b0febcc16
Hendrika Valeska Andhela <hvandhela@gmail.com>
2022-10-24 21:32:11 +0700

    update ; add category

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -8,7 +8,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <h6>Are u sure you want to delete this data?</h6>
+                <h6>Are you sure you want to delete this data?</h6>
             </div>
             <form wire:submit.prevent="destroyCategory">
                 <div class="modal-footer">
